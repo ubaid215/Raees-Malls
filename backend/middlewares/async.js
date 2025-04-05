4f88e1746e2e7d210d42d6e8307dbc799472e991
Muhammad Ubaidullah <ubaidtech274@gmail.com>
2025-04-05 18:19:36 +0500

    Folder Structure Setup

--- backend/middlewares/async.js
+++
@@ -1,7 +0,0 @@
-// backend/middlewares/async.js
-const asyncHandler = (fn) => (req, res, next) => {
-    Promise.resolve(fn(req, res, next)).catch(next);
-  };
-  
-  module.exports = asyncHandler;
-  
